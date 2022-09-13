5128820a465f925c4c6662d0e9e6a615ed6fa0ae
Israel Galvez <galvez.rli.51@gmail.com>
2022-09-13 14:06:02 -0700

    13/09/22

---
+++ 01.SitioWeb-BlogDeViajero/js/script.js
@@ -0,0 +1,12 @@
+// ==============================================
+//     BANNER
+// ==============================================
+
+$(".fade-slider").jdSlider({
+    isSliding: false,
+    isAuto: true,
+    isLoop: true,
+    isDrag: false,
+    interval: 5000,
+    speed: 1000
+});
---
+++ 01.SitioWeb-IsraPizzaBlog/js/script.js
@@ -0,0 +1,9 @@
+/* Banner */
+$(".fade-slider").jdSlider({
+    isSliding: false,
+    isAuto: true,
+    isLoop: true,
+    isDrag: false,
+    interval: 5000,
+    speed: 1000
+});
